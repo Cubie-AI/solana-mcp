bd443ba88586c8a98a13d6e40674313566e05cef
SolSoc <socrates.nft@gmail.com>
2025-04-24 16:31:38 -0700

    Fixing safeList methods

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -44,13 +44,13 @@ async function safeList<Method extends () => Promise<any>>(method: Method) {
 }
 
 export async function safeListResources(mcpClient: Client) {
-  return await safeList(mcpClient.listResources);
+  return await safeList(async () => await mcpClient.listResources());
 }
 
 export async function safeListTools(mcpClient: Client) {
-  return await safeList(mcpClient.listTools);
+  return await safeList(async () => await mcpClient.listTools());
 }
 
 export async function safeListPrompts(mcpClient: Client) {
-  return await safeList(mcpClient.listPrompts);
+  return await safeList(async () => await mcpClient.listPrompts());
 }
